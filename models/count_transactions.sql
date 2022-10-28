4266a3bda0d1ee8c166c6f46a2935b5f8b5ffa14
Nawfel <79513906+NawfelBC@users.noreply.github.com>
2022-10-28 20:17:18 +0200

    Delete count_transactions.sql

--- models/count_transactions.sql
+++
@@ -1,6 +0,0 @@
-{{config( materialized='table')}}
-
-SELECT CAST(staging_table.block_timestamp as DATE) as date, COUNT(*) as number_of_transactions
-FROM `astrafy-thc.dbt_nawfelbc.staging_table` as staging_table
-GROUP BY 1
-ORDER BY 1
