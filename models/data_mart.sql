b78295652f5c72dc6e242ebcb36a591be87fb6f9
Nawfel <79513906+NawfelBC@users.noreply.github.com>
2022-11-01 23:55:52 +0100

    Update data_mart.sql

--- models/data_mart.sql
+++ models/data_mart.sql
@@ -1,11 +1,10 @@
-{{config( materialized='table')}}
+{{config( materialized='table')}} -- This line allows our model to be rebuilt as a table on each run
 
 -- This table is created from the union of inputs and outputs.
--- Inputs are set negative so we can calculate the balance of each address by adding up (earnings + (- spendings))
 WITH inputs_outputs AS (
     SELECT
     array_to_string(inputs.addresses, ",") as address
-    , -inputs.value as value
+    , -inputs.value as value -- Inputs are set negative so we can calculate the balance of each address by adding up (earnings + (- spendings))
     , is_coinbase
     FROM `bigquery-public-data.crypto_bitcoin_cash.transactions`, UNNEST(inputs) as inputs -- We use UNNEST to flatten the array
     UNION ALL
@@ -20,6 +19,6 @@ SELECT
 address 
 , sum(value) as balance -- We do the sum of all values for each address to get its current balance
 FROM inputs_outputs
-WHERE is_coinbase = False -- We don't want to hold addresses that did at least one transaction via coinbase
+WHERE is_coinbase = False -- We filter out all addresses that did at least one transaction via coinbase
 GROUP BY 1
 ORDER BY balance DESC
