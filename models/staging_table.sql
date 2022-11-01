1cc6121773ff725acf693d104cbafbdb4685e9f8
Nawfel <79513906+NawfelBC@users.noreply.github.com>
2022-11-01 23:54:29 +0100

    added more comments

--- models/staging_table.sql
+++ models/staging_table.sql
@@ -1,6 +1,6 @@
-{{config( materialized='table')}}
+{{config( materialized='table')}} -- This line allows our model to be rebuilt as a table on each run
 
 SELECT *
 FROM `bigquery-public-data.crypto_bitcoin_cash.transactions` as transactions
 WHERE DATE(transactions.block_timestamp) BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 3 MONTH) AND CURRENT_DATE() -- We filter our data to keep just the last 3 months to this date
-ORDER BY transactions.block_timestamp
+ORDER BY transactions.block_timestamp -- We order the data by the timestamp in ascending order
